3a3b3d5417496b2c8e3b6dbde3443a64930d1b60
Rohan Jacob-Rao <rohanjr@gmail.com>
2015-11-26 21:13:30 -0500

    Added example to show lack of administrative redexes.

--- main.ml
+++ main.ml
@@ -1,9 +1,6 @@
 open Variable
 open Simple
 
-let u = U
-let cps_u = Simple2cps.cps_program_of_simple_term u
-
 (* Running example in Danvy's 3 steps paper *)
 let g = SrcVariable "g"
 let x = SrcVariable "x"
@@ -14,3 +11,8 @@ let named_eg = Simple2named.named_of_simple_term eg
 let sequential_eg = Named2sequential.sequential_of_named_term named_eg
 let cps_eg = Sequential2cps.cps_program_of_sequential_term sequential_eg
 let cps_eg' = Simple2cps.cps_program_of_simple_term eg (* same but with different variable names! *)
+
+(* Shows that output is free of administrative redexes *)
+let xx = Lam ((x, Unit), Var x)
+let w = App (xx, xx)
+let cps_w = Simple2cps.cps_program_of_simple_term w
