0f859987742c1aafb0acaeb0efb431fdc2cfe8f2
Rohan Jacob-Rao <rohanjr@gmail.com>
2015-12-08 15:04:30 -0500

    Comment about named form of language.

--- named.ml
+++ named.ml
@@ -5,6 +5,8 @@ type term =
   (* serious term: named application between terms *)
   (* let v = e1 e2 in tm *)
   | LetApp of app_variable * (term * term) * term
+  (* note: final term should just be the app_variable, but it is convenient to
+   * have it here explicitly for the recursive translation to sequential form *)
 
 and trivial =
   | U
